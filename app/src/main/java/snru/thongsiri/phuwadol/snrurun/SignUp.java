525923f884b912b9bc0081ab12b9e8020f2511e7
PHUWA <pooiiz.club@gmail.com>
2016-05-16 00:58:56 -0700

    finish Signup 50%

--- app/src/main/java/snru/thongsiri/phuwadol/snrurun/SignUp.java
+++ app/src/main/java/snru/thongsiri/phuwadol/snrurun/SignUp.java
@@ -2,12 +2,31 @@ package snru.thongsiri.phuwadol.snrurun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUp extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private RadioGroup radioGroup;
+    private RadioButton choice1RadioButton, choice2RadioButton, choice3RadioButton, choice4RadioButton, choice5RadioButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+
+
+    }//Main Mathod
+
+    public void clickSignUpSign(View view) {
+
     }
-}
+
+}//Main Class
