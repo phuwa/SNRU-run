2251fc8742aea59809accc8aa93f08d584e1e483
PHUWA <pooiiz.club@gmail.com>
2016-05-16 02:05:55 -0700

    AlertDialog finish

--- app/src/main/java/snru/thongsiri/phuwadol/snrurun/SignUp.java
+++ app/src/main/java/snru/thongsiri/phuwadol/snrurun/SignUp.java
@@ -1,5 +1,6 @@
 package snru.thongsiri.phuwadol.snrurun;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,9 @@ public class SignUp extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton choice1RadioButton, choice2RadioButton, choice3RadioButton, choice4RadioButton, choice5RadioButton;
 
+    private String nameString, userString, passwordString, avatarString;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +25,41 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         //Bind Widget
-
+        bindWidget();
 
     }//Main Mathod
 
+    private void bindWidget() {
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        radioGroup = (RadioGroup) findViewById(R.id.ragAvata);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        choice5RadioButton = (RadioButton) findViewById(R.id.radioButton5);
+
+
+    }
+
     public void clickSignUpSign(View view) {
+
+        //Get Value From EditText
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")){
+
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกให้ครบทุกช่องครับ");
+
+        }
+        else {
+
+        }
 
     }
 
